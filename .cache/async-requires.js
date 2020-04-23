629af6d9f4003167a6a19e901f03d1d1947d4748
talha915 <talhazafar915426@gmail.com>
2020-04-22 21:23:42 -0700

    Final

---
+++ .cache/async-requires.js
@@ -0,0 +1,18 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-app-js": () => import("./../src/pages/app.js" /* webpackChunkName: "component---src-pages-app-js" */),
+  "component---src-pages-boldpledge-js": () => import("./../src/pages/boldpledge.js" /* webpackChunkName: "component---src-pages-boldpledge-js" */),
+  "component---src-pages-boldpledgecommitment-js": () => import("./../src/pages/boldpledgecommitment.js" /* webpackChunkName: "component---src-pages-boldpledgecommitment-js" */),
+  "component---src-pages-boldseries-js": () => import("./../src/pages/boldseries.js" /* webpackChunkName: "component---src-pages-boldseries-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-mastercoachdetail-js": () => import("./../src/pages/mastercoachdetail.js" /* webpackChunkName: "component---src-pages-mastercoachdetail-js" */),
+  "component---src-pages-mastercoaches-js": () => import("./../src/pages/mastercoaches.js" /* webpackChunkName: "component---src-pages-mastercoaches-js" */),
+  "component---src-pages-membership-js": () => import("./../src/pages/membership.js" /* webpackChunkName: "component---src-pages-membership-js" */),
+  "component---src-pages-page-2-js": () => import("./../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-story-js": () => import("./../src/pages/story.js" /* webpackChunkName: "component---src-pages-story-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,21 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\404.js"))),
+  "component---src-pages-app-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\app.js"))),
+  "component---src-pages-boldpledge-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\boldpledge.js"))),
+  "component---src-pages-boldpledgecommitment-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\boldpledgecommitment.js"))),
+  "component---src-pages-boldseries-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\boldseries.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\index.js"))),
+  "component---src-pages-mastercoachdetail-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\mastercoachdetail.js"))),
+  "component---src-pages-mastercoaches-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\mastercoaches.js"))),
+  "component---src-pages-membership-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\membership.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\page-2.js"))),
+  "component---src-pages-story-js": hot(preferDefault(require("E:\\D\\ReactJs Projects\\Freelance\\Gatsby\\Bold\\gatsby-site\\src\\pages\\story.js")))
+}
+

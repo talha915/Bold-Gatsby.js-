67e01f3144c85c14478c38abc38d8acc0084b9e6
talha915 <talhazafar915426@gmail.com>
2020-04-23 13:53:22 -0700

    Oerview detail

--- src/components/MasterCoaches/MasterDetail.js
+++ src/components/MasterCoaches/MasterDetail.js
@@ -12,113 +12,126 @@ class MasterDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mastercoaches: ''
+            mastercoaches: '',
+            showType: 0
         }
     }
 
-    componentDidMount() {
-        this.setState({mastercoaches: JSON.parse(localStorage.getItem('selected'))});
+    showType;
+
+    UNSAFE_componentWillMount() {
+        this.setState({ mastercoaches: JSON.parse(localStorage.getItem('selected')) });
+    }
+
+    showOverview = () => {
+        this.setState({ showType: 1 });
+    }
+
+    headSet = () => {
+        let data = this.state.mastercoaches;
+        return (
+            <div className="head">
+                <div className="title-holder">
+                    <h2>{data.coachname}</h2>
+                    <span className="designation">{data.text}</span>
+                </div>
+                <div className="btn-holder">
+                    <a href="#" className="btn btn-border">Sample Play Lesson 1</a>
+                </div>
+            </div>
+        )
+    }
+
+    overView = () => {
+        let data = this.state.mastercoaches.Overview;
+        return (
+            <div>
+                <h3>{data.heading}</h3>
+                <p>{data.detail}</p>
+                <p>{data.detail1}</p>
+                <p>{data.detail2}</p>
+                <p>{data.detail3}</p>
+            </div>
+        )
+    }
+
+    getLessons = () => {
+        let data = this.state.mastercoaches.lesson.arrayoflesson;
+        let lessons = data.map((items, index) => {
+            return (
+                <article className="lesson-article" key={index}>
+                    <div className="content">
+                        <div className="img-box">
+                            <img src={items.image} width="264" height="185" alt="img description" />
+                            <span className="text">{items.timefram}</span>
+                        </div>
+                        <div className="text-box">
+                            <h3>{items.heading}</h3>
+                            <strong className="subtitle">{items.subheading}</strong>
+                            <p>{items.detail}</p>
+                        </div>
+                    </div>
+                </article>
+            )
+        })
+        return lessons;
+    }
+
+    getQA = () => {
+        let data = this.state.mastercoaches.qandaa.overviewarray;
+        let questions = data.map((items, index) => {
+            return (
+                <div className="que-ans">
+                    <h3>{items.heading}</h3>
+                    <p>{items.detail}</p>
+                </div>
+            )
+        })
+        return questions;
+    }
+
+    showSelected = () => {
+        let type = window.location.href.split("#")[1];
     }
 
     render() {
-        console.log("Here Data: ", this.state.mastercoaches);
         return (
-            <section class="coaches-info">
-                <img class="circle-shape" src="images/circle-img10.svg" width="505" height="505" alt="img description" />
-                <div class="container">
-                    <div class="section-header">
-                        <h1 class="heading-border">master<strong>coach</strong></h1>
+            <section className="coaches-info">
+                <img className="circle-shape" src="images/circle-img10.svg" width="505" height="505" alt="img description" />
+                <div className="container">
+                    <div className="section-header">
+                        <h1 className="heading-border">master<strong>coach</strong></h1>
                     </div>
-                    <div class="detail-holder">
-                        <div class="img-col">
-                            <div class="img-box">
-                                <img src="images/img22.jpg" width="385" height="385" alt="img description" />
+                    <div className="detail-holder">
+                        <div className="img-col">
+                            <div className="img-box">
+                                <img src={this.state.mastercoaches.coachimage} width="385" height="385" alt="img description" />
                             </div>
-                            <a href="#" class="btn-watch">Watch trailer</a>
+                            <a href="#" className="btn-watch">Watch trailer</a>
                         </div>
-                        <div class="text-col">
-                            <div class="head">
-                                <div class="title-holder">
-                                    <h2>Emily lee</h2>
-                                    <span class="designation">coach</span>
-                                </div>
-                                <div class="btn-holder">
-                                    <a href="#" class="btn btn-border">Sample Play Lesson 1</a>
-                                </div>
-                            </div>
-                            <div class="tabset-holder">
-                                <ul class="tabset">
-                                    <li class="active"><a href="#tab-01">Overview</a></li>
-                                    <li><a href="#tab-02">Lessons</a></li>
-                                    <li><a href="#tab-03">Q & A</a></li>
+                        <div className="text-col">
+                            {this.headSet()}
+                            <div className="tabset-holder">
+                                <ul className="tabset">
+                                    <li className={window.location.href.split("#")[1] == "tab-01" ? "active" : ""} ><a href="#tab-01" onClick={this.showSelected}>Overview</a></li>
+                                    <li className={window.location.href.split("#")[1] == "tab-02" ? "active" : ""}><a href="#tab-02" onClick={this.showSelected}>Lessons</a></li>
+                                    <li className={window.location.href.split("#")[1] == "tab-03" ? "active" : ""}><a href="#tab-03">Q & A</a></li>
                                 </ul>
-                                <div class="tab-content">
-                                    <div id="tab-01" style={{display: "none"}}>
-                                        <h3>Header for overview</h3>
-                                        <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna.</p>
-                                        <p>Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed.</p>
-                                        <p>Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna.</p>
-                                        <p>Mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed.</p>
-                                    </div>
-                                    <div id="tab-02" style={{display: "none"}}>
-                                        <article class="lesson-article">
-                                            <div class="content">
-                                                <div class="img-box">
-                                                    <img src="images/img09.jpg" width="264" height="185" alt="img description" />
-                                                    <span class="text">00:00:00 / 00:04:50</span>
-                                                </div>
-                                                <div class="text-box">
-                                                    <h3>Lorem ipsum dolor?</h3>
-                                                    <strong class="subtitle">LESSON 1</strong>
-                                                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.</p>
-                                                </div>
+                                <div className="tab-content">
+                                    {window.location.href.split("#")[1] == "tab-01" ?
+                                        <div id="tab-01" >
+                                            {this.overView()}
+                                        </div>
+                                        :
+                                        window.location.href.split("#")[1] == "tab-02" ?
+                                            <div id="tab-02">
+                                                {this.getLessons()}
                                             </div>
-                                        </article>
-                                        <article class="lesson-article">
-                                            <div class="content">
-                                                <div class="img-box">
-                                                    <img src="images/img10.jpg" width="264" height="185" alt="img description" />
-                                                    <span class="text">00:00:00 / 00:04:50</span>
-                                                </div>
-                                                <div class="text-box">
-                                                    <h3>Lorem ipsum dolor?</h3>
-                                                    <strong class="subtitle">LESSON 2</strong>
-                                                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.</p>
-                                                </div>
+                                            :
+                                            <div id="tab-03" >
+                                                {this.getQA()}
                                             </div>
-                                        </article>
-                                        <article class="lesson-article">
-                                            <div class="content">
-                                                <div class="img-box">
-                                                    <img src="images/img11.jpg" width="264" height="185" alt="img description" />
-                                                    <span class="text">00:00:00 / 00:04:50</span>
-                                                </div>
-                                                <div class="text-box">
-                                                    <h3>Lorem ipsum dolor?</h3>
-                                                    <strong class="subtitle">LESSON 2</strong>
-                                                    <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.</p>
-                                                </div>
-                                            </div>
-                                        </article>
-                                    </div>
-                                    <div id="tab-03" style={{display: "none"}}>
-                                        <div class="que-ans">
-                                            <h3>Lorem ipsum dolor?</h3>
-                                            <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.</p>
-                                        </div>
-                                        <div class="que-ans">
-                                            <h3>Etia, ullamcorper?</h3>
-                                            <p>Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.</p>
-                                        </div>
-                                        <div class="que-ans">
-                                            <h3>Maecenas malesuada elit?</h3>
-                                            <p>Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Ut molestie a, ultricies porta urna. Lorem ipsum dolor sit amet enim. </p>
-                                        </div>
-                                        <div class="que-ans">
-                                            <h3>Carabitur et ligula?</h3>
-                                            <p> Ut molestie a, ultricies porta urna.Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.</p>
-                                        </div>
-                                    </div>
+                                    }
                                 </div>
                             </div>
                         </div>
